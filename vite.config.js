d054b511962c3845ab7ce895e2ba2bb44361b96b
Giga_aja <gigaananta07@gmail.com>
2025-10-21 08:22:53 +0700

    solve error, fix bug, update fitur

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(),
