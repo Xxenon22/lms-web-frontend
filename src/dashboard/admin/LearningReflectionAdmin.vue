6db25fca2ebcb5788fe7b4e70b28e28dd55b1414
Giga_aja <gigaananta07@gmail.com>
2025-10-23 16:51:08 +0700

    Update feature : update search engine

--- src/dashboard/admin/LearningReflectionAdmin.vue
+++ src/dashboard/admin/LearningReflectionAdmin.vue
@@ -57,8 +57,7 @@ onMounted(async () => {
 <template>
     <div class="flex justify-between m-3">
         <h1 class="font-bold">Learning Reflection</h1>
-        <AutoComplete v-model="searchText" optionLabel="name" :suggestions="suggestions" @complete="searchGuru"
-            @item-select="onSelectGuru" @clear="onClear" placeholder="Search teacher..." />
+        <InputText v-model="searchText" @clear="onClear" placeholder="Search teacher..." />
     </div>
 
     <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-12">
