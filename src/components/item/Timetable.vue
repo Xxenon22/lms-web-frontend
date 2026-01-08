37fba1a8915027855f8f99879f1f0c7678e448fe
Giga_aja <gigaananta07@gmail.com>
2026-01-08 09:42:51 +0700

    bug fixing: retrieve materi

--- src/components/item/Timetable.vue
+++ src/components/item/Timetable.vue
@@ -10,7 +10,7 @@ const activeTab = ref("0");
 <template>
     <div class="m-10">
         <!-- Tabs -->
-        <div class="flex gap-3 mb-6 shadow-2xl p-5 rounded-xl">
+        <!-- <div class="flex gap-3 mb-6 shadow-2xl p-5 rounded-xl">
             <button class="px-5 py-2 rounded-xl font-medium transition-all" :class="activeTab === '0'
                 ? '!bg-[#008C95] text-white shadow'
                 : '!bg-[#94A4A4] text-white'" @click="activeTab = '0'">
@@ -28,13 +28,13 @@ const activeTab = ref("0");
                 : '!bg-[#94A4A4] text-white'" @click="activeTab = '2'">
                 Grade XI Timetable
             </button>
-        </div>
+        </div> -->
 
         <!-- Content Container -->
         <div class="bg-white p-5 border-transparent rounded-lg ">
 
             <div v-if="activeTab === '0'">
-                <h2 class="text-lg font-semibold mb-3">Teacher Timetable</h2>
+                <Tag class="text-lg font-semibold mb-3">Timetables</Tag>
                 <JadwalGuru />
             </div>
 
